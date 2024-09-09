927128b4dff13c2d6569d4c8a8e8cd7890af42ee
Krit Dat <datnhse170330@fpt.edu.vn>
2024-09-09 19:11:27 +0700

    ticket_resell_be project

---
+++ src/main/java/com/swd392/ticket_resell_be/TicketResellBeApplication.java
@@ -0,0 +1,13 @@
+package com.swd392.ticket_resell_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketResellBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketResellBeApplication.class, args);
+	}
+
+}
