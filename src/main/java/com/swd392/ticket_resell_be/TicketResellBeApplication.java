13aee0dc4df910cf1635402180274f4c82b05222
neru <ngocngocthuc@gmail.com>
2024-10-06 07:46:06 +0700

    Update TicketResellBeApplication.java

--- src/main/java/com/swd392/ticket_resell_be/TicketResellBeApplication.java
+++ src/main/java/com/swd392/ticket_resell_be/TicketResellBeApplication.java
@@ -9,7 +9,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TicketResellBeApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().directory("./").load();
+        Dotenv dotenv = Dotenv.configure().directory("/").load();
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
