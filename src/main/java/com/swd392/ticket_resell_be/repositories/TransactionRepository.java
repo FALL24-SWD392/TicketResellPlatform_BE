80cb954b2469e2868bfaeb94f67e446199c91142
Hoang Truong Minh <truonghoangprovip@gmail.com>
2024-10-21 13:05:40 +0700

    Fix: Pageable of Transaction

--- src/main/java/com/swd392/ticket_resell_be/repositories/TransactionRepository.java
+++ src/main/java/com/swd392/ticket_resell_be/repositories/TransactionRepository.java
@@ -1,13 +1,11 @@
 package com.swd392.ticket_resell_be.repositories;
 
-import com.swd392.ticket_resell_be.dtos.requests.PageDtoRequest;
 import com.swd392.ticket_resell_be.entities.Transaction;
 import com.swd392.ticket_resell_be.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
