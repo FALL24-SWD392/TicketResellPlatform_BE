8b2c06c6cf2e86a7b11a60f7641e31ef5c047496
Hoang Truong Minh <truonghoangprovip@gmail.com>
2024-11-07 13:02:14 +0700

    Add: API get Subscription for User

---
+++ src/main/java/com/swd392/ticket_resell_be/dtos/responses/SubscriptionDtoResponse.java
@@ -0,0 +1,23 @@
+package com.swd392.ticket_resell_be.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionDtoResponse {
+
+    private UUID id;
+    private String name;
+    private int saleLimit;
+    private String description;
+    private int pointRequired;
+    private float price;
+    private boolean isCurrentSubscription;
+}
