244f00b058c05423908c983332061af6fe812710
Hoang Truong Minh <truonghoangprovip@gmail.com>
2024-11-09 09:28:55 +0700

    Fix: Fix Response to FE (Payment)

--- src/main/java/com/swd392/ticket_resell_be/services/PaymentService.java
+++ src/main/java/com/swd392/ticket_resell_be/services/PaymentService.java
@@ -1,11 +1,14 @@
 package com.swd392.ticket_resell_be.services;
 
 import com.swd392.ticket_resell_be.dtos.responses.ApiItemResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 public interface PaymentService {
-    ApiItemResponse<String> handlePaymentCallback(
+    void handlePaymentCallback(
             String vnpAmount, String vnpBankCode, String vnpBankTranNo, String vnpCardType,
             String orderInfo, String vnpPayDate, String responseCode, String vnpTmnCode,
-            String vnpTransactionNo, String transactionStatus, String vnpTxnRef, String vnpSecureHash
+            String vnpTransactionNo, String transactionStatus, String vnpTxnRef, String vnpSecureHash,
+            HttpServletResponse response
     );
 }
